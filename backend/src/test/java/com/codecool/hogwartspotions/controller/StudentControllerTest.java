19e8b2cfb8a0eb37f5adf9371493d85ba2ff1553
charlotterepper <charlotte.repper01@gmail.com>
2023-03-31 19:10:06 +0200

    Finish StudentServiceTest

--- backend/src/test/java/com/codecool/hogwartspotions/controller/StudentControllerTest.java
+++ backend/src/test/java/com/codecool/hogwartspotions/controller/StudentControllerTest.java
@@ -38,41 +38,41 @@ public class StudentControllerTest {
     @Test
     void testGetAllStudentsOrderByFirstName() throws Exception {
         Room room = new Room(1L, "A", HouseType.GRYFFINDOR, 2, Set.of());
-        StudentDTO studentDTO = new StudentDTO(1L,"A", "B", HouseType.GRYFFINDOR, room, PetType.CAT);
+        StudentDTO studentDTO = new StudentDTO(1L, "A", "B", HouseType.GRYFFINDOR, room, PetType.CAT);
         when(studentService.getStudentsOrderByFirstName()).thenReturn(List.of(studentDTO));
 
         ResultActions resultActions = mvc.perform(get("/students"));
 
         resultActions.andExpect(status().isOk())
-            .andExpect(jsonPath("$[0].id", is(studentDTO.getId().intValue())))
-            .andExpect(jsonPath("$[0].firstName", is(studentDTO.getFirstName())))
-            .andExpect(jsonPath("$[0].lastName", is(studentDTO.getLastName())))
-            .andExpect(jsonPath("$[0].houseType", is(studentDTO.getHouseType().name())))
-            .andExpect(jsonPath("$[0].room.id", is(studentDTO.getRoom().getId().intValue())))
-            .andExpect(jsonPath("$[0].room.name", is(studentDTO.getRoom().getName())))
-            .andExpect(jsonPath("$[0].room.houseType", is(studentDTO.getRoom().getHouseType().name())))
-            .andExpect(jsonPath("$[0].room.capacity", is(studentDTO.getRoom().getCapacity())))
-            .andExpect(jsonPath("$[0].room.numberOfResidents", is(studentDTO.getRoom().getResidents().size())))
-            .andExpect(jsonPath("$[0].petType", is(studentDTO.getPetType().name())));
+                .andExpect(jsonPath("$[0].id", is(studentDTO.getId().intValue())))
+                .andExpect(jsonPath("$[0].firstName", is(studentDTO.getFirstName())))
+                .andExpect(jsonPath("$[0].lastName", is(studentDTO.getLastName())))
+                .andExpect(jsonPath("$[0].houseType", is(studentDTO.getHouseType().name())))
+                .andExpect(jsonPath("$[0].room.id", is(studentDTO.getRoom().getId().intValue())))
+                .andExpect(jsonPath("$[0].room.name", is(studentDTO.getRoom().getName())))
+                .andExpect(jsonPath("$[0].room.houseType", is(studentDTO.getRoom().getHouseType().name())))
+                .andExpect(jsonPath("$[0].room.capacity", is(studentDTO.getRoom().getCapacity())))
+                .andExpect(jsonPath("$[0].room.numberOfResidents", is(studentDTO.getRoom().getResidents().size())))
+                .andExpect(jsonPath("$[0].petType", is(studentDTO.getPetType().name())));
     }
 
     @Test
     void testAddStudent() throws Exception {
         Room room = new Room(1L, "A", HouseType.GRYFFINDOR, 2, Set.of());
-        StudentDTO studentDTO = new StudentDTO(1L,"A", "B", HouseType.GRYFFINDOR, room, PetType.CAT);
+        StudentDTO studentDTO = new StudentDTO(1L, "A", "B", HouseType.GRYFFINDOR, room, PetType.CAT);
         ObjectMapper objectMapper = new ObjectMapper();
 
         mvc.perform(post("/students/add")
                         .content(objectMapper.writeValueAsString(studentDTO))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()
-        );
+                );
     }
 
     @Test
     void testAddRoomToStudent() throws Exception {
         Room room = new Room(1L, "A", HouseType.GRYFFINDOR, 2, Set.of());
-        StudentDTO studentDTO = new StudentDTO(2L,"A", "B", HouseType.GRYFFINDOR, room, PetType.CAT);
+        StudentDTO studentDTO = new StudentDTO(2L, "A", "B", HouseType.GRYFFINDOR, room, PetType.CAT);
 
         mvc.perform(post("/students/" + studentDTO.getId() + "/" + room.getId()))
                 .andExpect(status().isOk()
